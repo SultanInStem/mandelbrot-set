b1d39ea73948c07b63ba18d2c98eb44cc437f48a
= <sultaninstem004@gmail.com>
2025-02-13 15:35:03 -0800

    minor

--- globals.py
+++ globals.py
@@ -1,11 +1,14 @@
 import math
 SCREEN_SIZE = (1200, 800)
 def to_math_coords(point): 
-    
-    pass 
+    x = point[0] - (SCREEN_SIZE[0] // 2)
+    y = (SCREEN_SIZE[1] // 2) - point[1]
+    return (x, y)
 
-def to_screen_coords(point): 
-    pass
+def to_screen_coords(point):
+    x = point[0] + (SCREEN_SIZE[0] // 2)
+    y = (SCREEN_SIZE[1] // 2) - point[1]
+    return (x, y) 
 
 
 def square_complex_num(number): 
